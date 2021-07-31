e68e4d1f1db372adc9d75a332f59b154e8b09298
shanthibabu <73182461+shanthi1999@users.noreply.github.com>
2021-07-31 12:32:53 +0530

    nodejs part -  backend

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const app = express();
+
+const routes = require('./routes');
+
+const PORT = process.env.PORT || 9000;
+
+
+app.use(express.json());
+
+app.use("/api",routes);
+
+app.listen(PORT, () => {
+  console.log(`port connected ${PORT}`);
+});
---
+++ routes.js
@@ -0,0 +1,29 @@
+const express = require("express");
+
+const routes = express.Router();
+const axios = require("axios");
+
+routes.post("/tokens", async (req, res) => {
+  try {
+    let captchaToken = req.body.token;
+
+    // Put your secret key here.
+    var secretKey = "6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe";
+
+    // req.connection.remoteAddress will provide IP address of connected user.
+
+    const url = `https://www.google.com/recaptcha/api/siteverify?secret=${secretKey}&response=${captchaToken}`;
+
+    const response = await axios.post(url);
+    const success = response.data;
+    console.log(response);
+
+    if (success) {
+      res.json({ status: true, result: sucess });
+    }
+  } catch (err) {
+    res.json({ status: false, result: "Not a valid token" });
+  }
+});
+
+module.exports = routes;
